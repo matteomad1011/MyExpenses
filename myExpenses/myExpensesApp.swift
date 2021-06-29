4223c1ec457e5209c8b7e274ae23ed3c3647c4ec
matteocavallo <mcavallo@soldo.com>
2021-06-29 20:38:19 +0200

    Initial Commit

---
+++ myExpenses/myExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  myExpensesApp.swift
+//  myExpenses
+//
+//  Created by Matteo Cavallo on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct myExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
